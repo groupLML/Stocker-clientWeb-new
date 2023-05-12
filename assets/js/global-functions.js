262d56587f46f5c39f0ddd8d188df176210c29b7
Lital Yehuda <lital5565@gmail.com>
2023-05-12 17:20:22 +0300

    global functions

---
+++ assets/js/global-functions.js
@@ -0,0 +1,21 @@
+﻿function renderUser(user) { //render active user
+    let str = `<h6> ${user.firstName} ${user.lastName}</h6>`;
+    str += `<span> ${user.position}</span>`;
+    document.getElementById("user_ph1").innerHTML += str;
+    str = `<span>שלום, ${user.firstName}</span>`;
+    document.getElementById("user_ph2").innerHTML += str;
+}
+
+
+function clearStorage() {
+    sessionStorage.clear();
+}
+
+
+function reload() {
+    var item = sessionStorage.getItem('Id');
+    if (item !== null) {
+        sessionStorage.removeItem('Id');
+        location.reload();
+    }
+}
