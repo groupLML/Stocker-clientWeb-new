963ffa6bc33f2e26cd200c39aa11c923e3ebf403
Lital Yehuda <lital5565@gmail.com>
2023-06-12 15:18:47 +0300

    שינויים קטנים

--- assets/js/global-functions.js
+++ assets/js/global-functions.js
@@ -56,26 +56,3 @@ function sweetalert(title, text, bool, isGoToPage, page) {
             }
         });
 }
-
-
-//function sweetalert(title, text, bool, isGoToPage, page) {
-//    swal({
-//        title: title,
-//        text: text,
-//        buttons: {
-//            confirm: {
-//                text: "OK",
-//                value: true,
-//                visible: true
-//            }
-//        },
-//        dangerMode: bool,
-//        closeOnClickOutside: false
-//    })
-//        .then(() => {
-//            if (isGoToPage) 
-//                window.location.href = page;
-//            else 
-//                return false;
-//        });
-//}
